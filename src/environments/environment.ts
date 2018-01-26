33c32fad7d43c1e00b7a85b1caff995d4021a187
arlenmendes <arlenmsilva@gmail.com>
2018-01-26 19:21:26 -0200

    corrigindo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,5 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyBXYwZ4BLLyflObNfDo_bUkniV5im1WI64',
-    authDomain: 'sd-feed.firebaseapp.com',
-    databaseURL: 'https://sd-feed.firebaseio.com',
-    projectId: 'sd-feed',
-    storageBucket: 'sd-feed.appspot.com',
-    messagingSenderId: '160772235690'
-  }
+  production: false
 };
